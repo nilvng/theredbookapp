d7ec0707a5cf0eaff602800609fa22a2c2d47cdd
Nil Nguyen <nil@blinq.me>
2023-04-15 14:14:23 +1000

    💄 (create-symposium): halfsheet modal with text field and button

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import IncomingCall from './views/incomingCall';
-
+import BasicModal from './Symposium/BasicModal';
 const testData = {
   name: 'Important Meeting',
   startDate: '2021-04-01T12:00:00Z',
@@ -9,27 +9,27 @@ const testData = {
     {
       id: 1,
       name: 'Speaker 1',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1356980987360591877/X3n_qwdq_400x400.jpg'},
+      avatar: { uri: 'https://pbs.twimg.com/profile_images/1356980987360591877/X3n_qwdq_400x400.jpg' },
     },
     {
       id: 2,
       name: 'Speaker 2',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1315124510689951744/xMft8kFx_400x400.png'},
+      avatar: { uri: 'https://pbs.twimg.com/profile_images/1315124510689951744/xMft8kFx_400x400.png' },
     },
     {
       id: 3,
       name: 'Speaker 3',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_400x400.jpg'},
+      avatar: { uri: 'https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_400x400.jpg' },
     },
     {
       id: 4,
       name: 'Speaker 4',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1372928334653444103/hoyp5vRt_400x400.jpg'},
+      avatar: { uri: 'https://pbs.twimg.com/profile_images/1372928334653444103/hoyp5vRt_400x400.jpg' },
     },
     {
       id: 5,
       name: 'Speaker 5',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1330811947026055168/3u8TsJde_400x400.jpg'},
+      avatar: { uri: 'https://pbs.twimg.com/profile_images/1330811947026055168/3u8TsJde_400x400.jpg' },
     }
   ],
 }
@@ -38,7 +38,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <IncomingCall space={testData}></IncomingCall>
-      <StatusBar style="auto" />
+      <BasicModal />
     </View>
   );
 }
