104142349abd127c80a9c5d80333fb4518542518
Nil Nguyen <nil@blinq.me>
2023-04-16 21:57:29 +1000

    🚑️ using string for number
    remove "", improve padding, margin, add spacer

--- views/incomingCall.js
+++ views/incomingCall.js
@@ -1,20 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native';
-import { VStack, Box, Divider, Avatar, HStack, Button } from "@react-native-material/core";
+import { VStack, Box, Divider, Avatar, HStack, Button, Spacer } from "@react-native-material/core";
 
 
 export default function IncomingCall(props) {
   return (
     <View style={styles.container}>
-      <VStack m={"4"} spacing={"2"}>
+      <VStack m={4} spacing={2}>
         {/* Title */}
-        <Box mt={"4"} pb={"3"}>
+        <Box mt={20} pb={3}>
           <Text style={styles.title}>'{props.space.name}' has Started</Text>
           <Text style={styles.subtitle}>{props.space.speakers.length} speaker{props.space.speakers.length == 1 ? '' : 's'}</Text>
         </Box>
         {/* Avatars */}
         <HStack style={{
           justifyContent: 'space-evenly',
-          zIndex: 2,
         }}>
           {props.space.speakers.slice(0, 4).map((speaker) => (
             <Avatar key={speaker.id} image={speaker.avatar}></Avatar>
@@ -22,8 +21,9 @@ export default function IncomingCall(props) {
         </HStack>
         {/* Platform effect */}
         <Box style={styles.platform} />
+        <Spacer />
         {/* Buttons */}
-        <VStack m={"4"} spacing={"1"}>
+        <VStack m={4} spacing={8}>
           <Button style={styles.button} title="Join" color="#66bb6a" />
           <Button style={styles.button} title="Ignore" color="#f44336" />
         </VStack>
@@ -34,28 +34,28 @@ export default function IncomingCall(props) {
 
 const styles = StyleSheet.create({
   title: {
-    fontSize: "2",
+    fontSize: 18,
     color: '#FFFFFF',
     textAlign: 'center',
   },
   subtitle: {
-    fontSize: "1",
+    fontSize: 1,
     color: '#FFFFFF',
     textAlign: 'center',
   },
   button: {
-    padding: "0.5"
+    padding: 4
   },
   platform: {
     backgroundColor: "transparent",
     alignSelf: 'stretch',
     borderColor: "transparent",
-    borderBottomWidth: "1",
+    borderBottomWidth: 1,
     borderBottomColor: "#404040",
-    borderLeftWidth: "20",
-    borderRightWidth: "20",
-    marginHorizontal: "5",
-    marginTop: "-7",
+    borderLeftWidth: 20,
+    borderRightWidth: 20,
+    marginHorizontal: 5,
+    marginTop: 28,
     zIndex: 1,
   },
   container: {
@@ -63,5 +63,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#303030',
     color: '#FFFFFF',
     alignItems: 'center',
+    paddingVertical: 50,
   },
 });
