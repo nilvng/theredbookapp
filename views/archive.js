cb297a87a07b0f83fa705a9c97973e2adc94adc1
NicholasKabissios <s3840379@student.rmit.edu.au>
2023-04-17 14:51:58 +1000

    💄Populate archive page with basic outline

--- views/archive.js
+++ views/archive.js
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { VStack, Box, Divider, Avatar, HStack, Button, Spacer } from "@react-native-material/core";
+import { StyleSheet, Text, View, SectionList } from 'react-native';
+import { VStack, Button, } from "@react-native-material/core";
 
+const dummyPodcasts = [
+    'Podcast #1 - Welcome to our podcast!',
+    'Podcast #2 - Heated discussion with the hosts',
+    'Podcast #3 - Viewer Q&A',
+]
+
+const dummyDiscusions = [
+    'Ex-Amazon workers, share you experiences!',
+    'What can be done about crunch time in the game-dev industry?',
+]
 
 export default function Archive({ navigation }) {
     return (
         <View style={styles.container}>
+            <Text style={styles.title}>ARCHIVE</Text>
+            <SectionList
+                sections={[
+                    { title: 'Podcasts', data: dummyPodcasts },
+                    { title: 'Discussions', data: dummyDiscusions },
+                ]}
+                renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+                renderSectionHeader={({ section }) => (
+                    <Text style={styles.sectionHeader}>{section.title}</Text>
+                )}
+                keyExtractor={item => `basicListEntry-${item}`}
+            />
+
             <VStack m={4} spacing={2}>
                 <Button style={styles.button} title="Go back" color="blue"
                     onPress={() => navigation.navigate('Call')} />
-
             </VStack>
         </View>
     )
@@ -17,9 +39,10 @@ export default function Archive({ navigation }) {
 
 const styles = StyleSheet.create({
     title: {
-        fontSize: 18,
+        fontSize: 50,
         color: '#FFFFFF',
         textAlign: 'center',
+        fontWeight: 'bold',
     },
     subtitle: {
         fontSize: 1,
@@ -47,5 +70,21 @@ const styles = StyleSheet.create({
         color: '#FFFFFF',
         alignItems: 'center',
         paddingVertical: 50,
+    },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 30,
+        fontWeight: 'bold',
+        backgroundColor: 'grey',
+        color: 'white'
+    },
+    item: {
+        backgroundColor: 'transparent',
+        fontSize: 20,
+        color: 'white',
+        padding: 5,
     },
 });
