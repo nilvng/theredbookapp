9fc28a378a233abc7bc5836b84f31079296b92c3
Nil Nguyen <nil@blinq.me>
2023-04-17 22:22:24 +1000

    🐛 keyboard hide views, buggy touchableOpacity
    remove TouchableOpacity, using KeyboardAvodingView to move items up when keyboard appears

--- Symposium/BasicModal.jsx
+++ Symposium/BasicModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Modal, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Button, Flex, Stack, TextInput, IconButton } from "@react-native-material/core";
+import { Modal, StyleSheet, Text, View, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { Button, Flex, Stack, TextInput, IconButton, Spacer } from "@react-native-material/core";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 const BasicModal = () => {
@@ -13,48 +13,52 @@ const BasicModal = () => {
                 transparent={true}
                 visible={modalVisible}
                 onBackdropPress={() => setModalVisible(false)}
-                // presentationStyle='pageSheet' // iOS only
                 onRequestClose={() => {
-                    setModalVisible(!modalVisible);
-                }}>
-                <TouchableOpacity
-                    style={styles.modalContainer}
-                    onPress={() => setModalVisible(false)}
-                    onPressOut={() => setModalVisible(false)}
-                    activeOpacity={1}>
-                    <View style={styles.modalView}>
-                        <Text style={styles.modalTitle}>Have your say!</Text>
-                        <Stack fill spacing={8} style={{ width: "100%", marginTop: 16 }}>
-                            <TextInput
-                                style={{ width: "100%" }}
-                                variant='outlined'
-                                label='Enter your subject line'
-                                keyboardType='default'
-                            />
-                        </Stack>
-                    <Flex style={{ width: "100%", position: 'absolute', bottom: 0, marginBottom: 16 }}
-                        direction='row' justify='evenly' fill wrap="nowrap" spacing={18}>
-                        <Button
-                            style={[styles.button, styles.buttonClose]}
-                            onPress={() => setModalVisible(!modalVisible)}
-                            variant='text'
-                            title="Submit"
-                            tintColor='white'
-                        />
-                        <IconButton
-                            onPress={() => setModalVisible(!modalVisible)}
-                            icon={props => <Icon name='clock' {...props} />}
-                        />
-                    </Flex>
-                    </View>
-                </TouchableOpacity>
+                    setModalVisible(!modalVisible)
+                }}
+            >
+                <View
+                    style={styles.modalContainer}>
+                    <KeyboardAvoidingView
+                        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                        style={{ flex: 1 }}
+                    >
+                        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                            <View style={styles.modalView}>
+                                <Text style={styles.modalTitle}>Have your say!</Text>
+                                <Stack fill spacing={8} style={{ width: "100%", marginTop: 16 }}>
+                                    <TextInput
+                                        style={{ width: "100%" }}
+                                        variant='outlined'
+                                        label='Enter your subject...'
+                                        keyboardType='default'
+                                    />
+                                </Stack>
+                                <Flex style={{ width: "100%", position: 'absolute', bottom: 0, marginBottom: 16 }}
+                                    direction='row' justify='evenly' fill wrap="nowrap" spacing={18}>
+                                    <Button
+                                        style={[styles.buttonClose]}
+                                        onPress={() => setModalVisible(!modalVisible)}
+                                        variant='text'
+                                        title="Submit"
+                                        color='black'
+                                    />
+                                    <IconButton
+                                        onPress={() => setModalVisible(!modalVisible)}
+                                        icon={props => <Icon name='clock' {...props} />}
+                                    />
+                                </Flex>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </KeyboardAvoidingView>
+                </View>
             </Modal >
 
             <Button
                 style={[styles.button, styles.buttonOpen]}
-                onPress={() => setModalVisible(true)}
+                onPress={() => setModalVisible(!modalVisible)}
                 variant='text'
-                title="Open Modal"
+                title="Host symposium"
             />
         </View >
     );
@@ -88,6 +92,7 @@ const styles = StyleSheet.create({
     },
     button: {
         width: '100%',
+        padding: 4
     },
     buttonOpen: {
         backgroundColor: '#FFF4F1',
@@ -104,13 +109,11 @@ const styles = StyleSheet.create({
     },
     modalText: {
         marginBottom: 15,
-        fontFamily: 'Roboto',
         textAlign: 'center',
     },
     modalTitle: {
         fontSize: 20,
         marginVertical: 12,
-        fontFamily: 'Roboto',
         textAlign: 'center',
     },
 });
