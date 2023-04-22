8058c2dea186498b692e32f5ba3ef5a39dc634d9
NicholasKabissios <s3840379@student.rmit.edu.au>
2023-04-22 20:21:08 +1000

    💄Style the cards a bit more

--- components/card.js
+++ components/card.js
@@ -3,28 +3,23 @@ import { StyleSheet, View } from 'react-native';
 
 export default function Card(props) {
     return (
-      <View style={styles.card}>
-        <View style={styles.cardContent}>
-          { props.children }
+        <View style={styles.card}>
+            <View style={styles.cardContent}>
+                {props.children}
+            </View>
         </View>
-      </View>
     );
-  }
+}
 
 const styles = StyleSheet.create({
     card: {
-      borderRadius: 6,
-      elevation: 3,
-      backgroundColor: '#fff',
-      shadowOffset: { width: 1, height: 1 },
-      shadowColor: '#333',
-      shadowOpacity: 0.3,
-      shadowRadius: 2,
-      marginHorizontal: 4,
-      marginVertical: 6,
+        borderRadius: 10,
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#FFF4F1',
     },
     cardContent: {
-      marginHorizontal: 18,
-      marginVertical: 20,
+        paddingTop: '10%',
+        paddingBottom: '10%',
     }
-  });
+});
