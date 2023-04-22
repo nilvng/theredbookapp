598dbbe1ddabfc3b340e88bb2125ee4ec91ba62b
jeremygilbertc <jeremy.gilbert.bm@gmail.com>
2023-04-23 01:21:01 +1000

    app.js update

--- App.js
+++ App.js
@@ -1,44 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import IncomingCall from './views/incomingCall';
-
-const testData = {
-  name: 'Important Meeting',
-  startDate: '2021-04-01T12:00:00Z',
-  speakers: [
-    {
-      id: 1,
-      name: 'Speaker 1',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1356980987360591877/X3n_qwdq_400x400.jpg'},
-    },
-    {
-      id: 2,
-      name: 'Speaker 2',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1315124510689951744/xMft8kFx_400x400.png'},
-    },
-    {
-      id: 3,
-      name: 'Speaker 3',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_400x400.jpg'},
-    },
-    {
-      id: 4,
-      name: 'Speaker 4',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1372928334653444103/hoyp5vRt_400x400.jpg'},
-    },
-    {
-      id: 5,
-      name: 'Speaker 5',
-      avatar: {uri: 'https://pbs.twimg.com/profile_images/1330811947026055168/3u8TsJde_400x400.jpg'},
-    }
-  ],
-}
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import ChatBox from './chatBox';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <IncomingCall space={testData}></IncomingCall>
-      <StatusBar style="auto" />
+      <ChatBox />
     </View>
   );
 }
@@ -46,8 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#303030',
-    color: '#FFFFFF',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
