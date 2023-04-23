e6e16bcd4614b611c3c646b7a92bc59192b737bc
Nil Nguyen <nil@blinq.me>
2023-04-23 11:10:34 +1000

    🚧 (local-storage): persist entered symposium data: title, topic, date, speaker

--- App.js
+++ App.js
@@ -5,10 +5,30 @@ import IncomingCall, { testData } from './views/incomingCall';
 import { NavigationContainer } from "@react-navigation/native";
 import { CardStyleInterpolators, createStackNavigator } from "@react-navigation/stack";
 import CreateModal from './Symposium/Views/CreateModal';
+import { useState, useCallback, useEffect } from 'react';
+import { createTable, getAll } from './Symposium/Models/symposium-db';
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [spaces, setSpace] = useState([])
+
+  const loadData = useCallback(async () => {
+    try {
+      await createTable()
+      getAll().then((data) => {
+        console.log(data);
+        setSpace(data);
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{
