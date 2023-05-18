3a2afcbcf91118938a772d860f16cdfb582aa511
Nil Nguyen <nil@blinq.me>
2023-05-18 22:28:49 +1000

    💄 (chatbox): update avatar

--- components/Message.js
+++ components/Message.js
@@ -2,17 +2,9 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
 import MessageBubble from './MessageBb';
-
-const Message = ({ content, isMyMessage }) => {
+import { Avatar } from 'react-native-paper';
+const Message = ({ content, isMyMessage, userName }) => {
   const styles = StyleSheet.create({
-    iconContainer: {
-      width: 28,
-      height: 28,
-      borderRadius: 14,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginRight: 8,
-    },
     messageContainer: {
       flexDirection: isMyMessage ? 'row-reverse' : 'row',
       alignItems: 'center',
@@ -23,9 +15,8 @@ const Message = ({ content, isMyMessage }) => {
 
   return (
     <View style={styles.messageContainer}>
-      <View style={[styles.iconContainer, { backgroundColor: isMyMessage ? '#0084ff' : '#eee' }]}>
-        <MaterialCommunityIcons name="account" size={24} color={isMyMessage ? '#fff' : '#000'} />
-      </View>
+      {!isMyMessage && userName != null && <Avatar.Text size={40} label={userName} />}
+      {userName == null && <Avatar.Text size={40} label="XD" />}
       <MessageBubble isMyMessage={isMyMessage}>
         <Text style={{ color: isMyMessage ? '#fff' : '#000' }}>{content}</Text>
       </MessageBubble>
