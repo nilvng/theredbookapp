09a3a278778f2b3ba0dbc98ec04c374368716c8a
Nil Nguyen <nil@blinq.me>
2023-05-18 14:42:54 +1000

    rename folder

---
+++ pages/archive.js
@@ -0,0 +1,173 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Image, TouchableOpacity, Modal } from 'react-native';
+import { VStack, Button, HStack } from "@react-native-material/core";
+import Card from '../components/card';
+
+const symposiums = [
+    {category: 'podcasts', data: dummyPodcasts},
+]
+
+const dummyPodcasts = [
+    { title: 'Podcast #1', description: 'Welcome to our podcast!', host: 'Nick' },
+    { title: 'Podcast #2', description: 'Heated discussion with the hosts', host: 'Nick' },
+    { title: 'Podcast #3', description: 'Viewer Q&A', host: 'Nick' },
+    { title: 'Dummy Podcast 1', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 2', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 3', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 4', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 5', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 6', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 7', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 8', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 9', description: 'Dummy podcast', host: 'Nick' },
+    { title: 'Dummy Podcast 10', description: 'Dummy podcast', host: 'Nick' }
+]
+
+const dummyDiscussions = [
+    { title: 'Ex-Amazon workers, share you experiences!', description: 'Discussion on prior experiences of amazon warehouse workers' },
+    { title: 'Crunch time in the game-dev industry?', description: 'Discussion on what can be done about crunch time in the game development industry' },
+]
+
+
+
+export default function Archive({ navigation }) {
+    const [modalVisible, setModalVisible] = useState(false);
+    const [pressed, setPressed] = useState(false);
+    const [currentData, setData] = useState("")
+
+    const Separator = () => {
+        return <View style={styles.separator} />;
+    };
+
+    const renderHeader = (header) => {
+        return (
+            <View>
+                <Text style={styles.sectionHeader}>{header}</Text>
+                <Separator />
+            </View>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}>
+
+                <View style={styles.modalView}>
+                    <View style={styles.container}>
+                        <Text style={styles.modalTitle}>{currentData.title}</Text>
+                        <Text>{currentData.description}</Text>
+                    </View>
+
+                    <View>
+                        <Button style={styles.button} title="Back" color="purple"
+                            onPress={() => setModalVisible(!modalVisible)} />
+                    </View>
+                </View>
+            </Modal>
+
+            <HStack>
+                <Text style={styles.title}>Archive</Text>
+            </HStack>
+
+            <FlatList
+                style={{ width: '95%' }}
+                data={dummyPodcasts}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => { setPressed(!pressed); setModalVisible(!modalVisible); setData(item) }}>
+                        <Card>
+                            <View style={styles.cardTitleContainer}>
+                                <Text style={styles.cardTitle}> {item.title}</Text>
+                            </View>
+                            <View style={styles.cardHostContainer}>
+                                <Text style={styles.cardHost}> {item.host}</Text>
+                            </View>
+                        </Card>
+                    </TouchableOpacity>)}
+                ItemSeparatorComponent={<Separator />}
+                ListHeaderComponent={renderHeader('Podcasts')}
+            />
+
+
+            <HStack>
+                <Button style={[styles.button]} title="Go back" color="purple"
+                    onPress={() => navigation.navigate('Call')} />
+            </HStack>
+
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 50,
+        color: '#FFFFFF',
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+    subtitle: {
+        fontSize: 1,
+        color: '#FFFFFF',
+        textAlign: 'center',
+    },
+    button: {
+        padding: 4
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#303030',
+        color: '#FFFFFF',
+        alignItems: 'center',
+        paddingVertical: 25,
+    },
+    // Headers in the list view
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 30,
+        fontWeight: 'bold',
+        backgroundColor: 'grey',
+        color: 'white'
+    },
+    modalView: {
+        flex: 1,
+        paddingTop: '60%',
+        height: '40%',
+    },
+    modalTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+        textAlign: 'center',
+    },
+    separator: {
+        height: 10,
+        width: '100%',
+    },
+    cardTitleContainer: {
+        flexGrow: 1,
+    },
+    cardTitle: {
+        fontSize: 15,
+        paddingLeft: 10,
+        alignSelf: 'baseline',
+    },
+    cardHostContainer: {
+        height: '30%',
+        borderRadius: 10,
+        backgroundColor: '#F3EED9',
+    },
+    cardHost: {
+        paddingHorizontal: 10,
+
+    },
+});
---
+++ pages/incomingCall.js
@@ -0,0 +1,102 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { VStack, Box, Divider, Avatar, HStack, Button, Spacer } from "@react-native-material/core";
+
+const testData = {
+  name: 'Important Meeting',
+  startDate: '2021-04-01T12:00:00Z',
+  speakers: [
+    {
+      id: 1,
+      name: 'Speaker 1',
+      avatar: {uri: 'https://pbs.twimg.com/profile_images/1356980987360591877/X3n_qwdq_400x400.jpg'},
+    },
+    {
+      id: 2,
+      name: 'Speaker 2',
+      avatar: {uri: 'https://pbs.twimg.com/profile_images/1315124510689951744/xMft8kFx_400x400.png'},
+    },
+    {
+      id: 3,
+      name: 'Speaker 3',
+      avatar: {uri: 'https://pbs.twimg.com/profile_images/1590968738358079488/IY9Gx6Ok_400x400.jpg'},
+    },
+    {
+      id: 4,
+      name: 'Speaker 4',
+      avatar: {uri: 'https://pbs.twimg.com/profile_images/1372928334653444103/hoyp5vRt_400x400.jpg'},
+    },
+    {
+      id: 5,
+      name: 'Speaker 5',
+      avatar: {uri: 'https://pbs.twimg.com/profile_images/1330811947026055168/3u8TsJde_400x400.jpg'},
+    }
+  ],
+}
+
+export default function IncomingCall( { navigation } ) {
+  return (
+    <View style={styles.container}>
+      <VStack m={4} spacing={2}>
+        {/* Title */}
+        <Box mt={20} pb={3}>
+          <Text style={styles.title}>'{testData.name}' has Started</Text>
+          <Text style={styles.subtitle}>{testData.speakers.length} speaker{testData.speakers.length == 1 ? '' : 's'}</Text>
+        </Box>
+        {/* Avatars */}
+        <HStack style={{
+          justifyContent: 'space-evenly',
+        }}>
+          {testData.speakers.slice(0, 4).map((speaker) => (
+            <Avatar key={speaker.id} image={speaker.avatar}></Avatar>
+          ))}
+        </HStack>
+        {/* Platform effect */}
+        <Box style={styles.platform} />
+        <Spacer />
+        {/* Buttons */}
+        <VStack m={4} spacing={8}>
+          <Button style={styles.button} title="Join" color="#66bb6a" />
+          <Button style={styles.button} title="Ignore" color="#f44336" />
+          <Button style={styles.button} title="Archive" color="purple" 
+          onPress={() => navigation.navigate('Archive')}
+          />
+        </VStack>
+      </VStack>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 1,
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  button: {
+    padding: 4
+  },
+  platform: {
+    backgroundColor: "transparent",
+    alignSelf: 'stretch',
+    borderColor: "transparent",
+    borderBottomWidth: 1,
+    borderBottomColor: "#404040",
+    borderLeftWidth: 20,
+    borderRightWidth: 20,
+    marginHorizontal: 5,
+    marginTop: 28,
+    zIndex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#303030',
+    color: '#FFFFFF',
+    alignItems: 'center',
+    paddingVertical: 50,
+  },
+});
