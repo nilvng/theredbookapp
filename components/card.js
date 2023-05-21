19d62da0e838e2809fa62f5a24057f0dbf762d95
Nil Nguyen <nil@blinq.me>
2023-05-21 18:03:17 +1000

    💄 (card-view): improve texts, button

--- components/card.js
+++ components/card.js
@@ -1,24 +1,27 @@
+import { HStack } from '@react-native-material/core';
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { Button } from 'react-native-paper';
 
-export default function Card(props) {
+export default function Card({ data }) {
+    const TextStackView = <View style={styles.textStack}>
+        <Text style={styles.hostText}> {data.host} hosted</Text>
+        {data.startDate != null ? <Text style={styles.text}>{data.startDate}</Text> : null}
+    </View>;
     return (
         <View style={styles.card}>
             <View style={styles.cardContent}>
                 <View style={styles.cardTitleContainer}>
-                    <Text style={styles.cardTitle}> {props.data.title}</Text>
+                    <Text style={styles.cardTitle}> {data.title}</Text>
+                    <Text style={styles.footerText}> {data.topic}</Text>
                 </View>
-                <View style={styles.cardHostContainer}>
-                    <Text style={styles.cardHost}> {props.data.host}</Text>
-                
-                    <Text style={styles.cardHost}> {props.data.topic}</Text>
-                
-                    <Text style={styles.cardHost}> {props.data.status}</Text>
-                
-                    <Text style={styles.cardHost}> {props.data.date}</Text>
-                
-                    <Text style={styles.cardHost}> {props.data.approved}</Text>
+                <View style={styles.detailContainer}>
+                    <HStack>
+                        {TextStackView}
+                        {Date.parse(data.startDate) < Date.now() ? <Button>Join</Button> : <Button>Remind</Button>}
+                    </HStack>
                 </View>
+
             </View>
         </View>
     );
@@ -35,21 +38,35 @@ const styles = StyleSheet.create({
     cardContent: {
         flex: 1,
     },
-        cardTitleContainer: {
+    footerText: {
+        fontSize: 14,
+        color: '#2e2e2d',
+
+    },
+    cardTitleContainer: {
+        padding: 10,
         flexGrow: 1,
     },
     cardTitle: {
         fontSize: 24,
-        paddingLeft: 10,
         alignSelf: 'baseline',
     },
-    cardHostContainer: {
-        height: '30%',
+    detailContainer: {
         borderRadius: 10,
         backgroundColor: '#F3EED9',
     },
-    cardHost: {
-        paddingHorizontal: 10,
-
+    text: {
+        fontSize: 14,
+    },
+    hostText: {
+        fontSize: 14,
+    },
+    textStack: {
+        flex: 1,
+        flexGrow: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        padding: 10,
     },
 });
