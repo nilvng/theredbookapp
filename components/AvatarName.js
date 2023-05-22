adcfd5fd9cb6e6c220007f2951910801b470eb09
Nil Nguyen <nil@blinq.me>
2023-05-22 20:28:07 +1000

    🩹 (AvatarName): crash when parse empty name

--- components/AvatarName.js
+++ components/AvatarName.js
@@ -5,7 +5,7 @@ import { View, Text } from 'react-native'
 export default function AvatarName({ name, image }) {
     return (
         <View style={{ alignItems: "center", marginTop: 4 }}>
-            {image == null && <Avatar.Text size={45} label={name[0].toUpperCase()} />}
+            {image == null && name != null && <Avatar.Text size={45} label={name[0].toUpperCase()} />}
             {image != null && <Avatar.Image size={45} source={{ uri: image }} />}
             <Text style={{ marginTop: 6 }}> {name} </Text>
         </View>
