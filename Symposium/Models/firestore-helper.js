15436ca8c08f61244de27e34db93ba33aea4f846
SuperSonic16 <25761885+thesupersonic16@users.noreply.github.com>
2023-05-29 20:28:13 +1000

    Firestore User Stuff

--- Symposium/Models/firestore-helper.js
+++ Symposium/Models/firestore-helper.js
@@ -1,4 +1,4 @@
-import { db } from "../../firebase.config";
+import { auth, db } from "../../firebase.config";
 
 const collectionName = "symposiums";
 
@@ -18,4 +18,43 @@ export const addSymposium = async (symposium) => {
         .then((snapshot) => {
             return snapshot.get();
         })
+}
+
+export const createUser = async (email, password, name, callback) => {
+    await auth.createUserWithEmailAndPassword(email, password)
+        .catch((error) => {
+            callback(error);
+        }).then((response) => {
+            db.collection("users").doc(response.user.uid).set({
+                name: name,
+            }).then(() => {
+                if (!response || !response.user) {
+                    return;
+                }
+                callback({ firebaseUser: response.user, userData: { name: name, image: null } });
+            }).catch((error) => {
+                callback(error);
+            });
+        });
+}
+
+export const loginUser = async (email, password, callback) => {
+    await auth.signInWithEmailAndPassword(email, password)
+        .catch((error) => {
+            callback(error);
+            console.log(error);
+        }).then((response) => {
+            if (!response || !response.user) {
+                return;
+            }
+            db.collection("users").doc(response.user.uid).get().then((doc) => {
+                if (doc.exists) {
+                    callback({ firebaseUser: response.user, userData: doc.data() });
+                } else {
+                    callback({ firebaseUser: response.user, userData: { name: email, image: null } });
+                }
+            }).catch((error) => {
+                callback(error);
+            });
+        });
 }
