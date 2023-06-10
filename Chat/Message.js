bd475a3d5142af1b7e4bee3e3285801536f8ee08
Nil Nguyen <nil@blinq.me>
2023-06-10 22:33:56 +1000

    🐛 (chat): Fix avatar text color
    use the default white color

--- Chat/Message.js
+++ Chat/Message.js
@@ -18,7 +18,7 @@ const Message = ({ item, isMyMessage, onVote }) => {
     <VStack style={{ alignSelf: isMyMessage ? "flex-end" : "flex-start" }}>
       <View style={styles.bubbleContainer}>
         {userName != null && <Avatar.Text size={40} label={userName[0].toUpperCase()} style={{ backgroundColor: isMyMessage ? "red" : "purple" }} />}
-        {userName == null && <Avatar.Text size={40} label="XD" color={isMyMessage ? "red" : "purple"} />}
+        {userName == null && <Avatar.Text size={40} label="XD" />}
         <MessageBubble isMyMessage={isMyMessage}>
           <Text style={{ color: '#000' }}>{content}</Text>
         </MessageBubble>
